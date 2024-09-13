a75845ad2cb605121ef1e4936897c68569b79be5
em4n0n <58372945+em4n0n@users.noreply.github.com>
2024-09-13 15:30:18 -0700

    Use login_required to only allow access for logged users

--- myproject/myapp/views.py
+++ myproject/myapp/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
 from django.core.exceptions import PermissionDenied
-
+from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
 # Create your views here.
 
 def myview(request):
     if request.user.is_anonymous():
         raise PermissionDenied()
+    
+@login_required
+def myview(request):
+    return HttpResponse("Hello World")
