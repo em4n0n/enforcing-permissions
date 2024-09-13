9dc302a6668eab350ba676777fc327a14ec33027
em4n0n <58372945+em4n0n@users.noreply.github.com>
2024-09-13 13:44:57 -0700

    Create Product model with Meta class permissions

--- myproject/myapp/models.py
+++ myproject/myapp/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Product(models.Model):
+    ProductID = models.IntegerField()
+    name = models.TextField()
+    category = models.TextField()
+    class Meta:
+        permissions = [('can_change_category', 'Can change category')]
