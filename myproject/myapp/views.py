ad2ca1d2867de9a023762b47d7bfb8937a659341
em4n0n <58372945+em4n0n@users.noreply.github.com>
2024-09-13 15:42:20 -0700

    Comment out code for next example

--- myproject/myapp/views.py
+++ myproject/myapp/views.py
@@ -2,18 +2,28 @@ from django.shortcuts import render
 from django.core.exceptions import PermissionDenied
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.decorators import user_passes_test
+from django.contrib.auth.decorators import permission_required
+
 # Create your views here.
 
-def myview(request): #permission denied if anonymous user
-    if request.user.is_anonymous():
-        raise PermissionDenied()
+# def myview(request): #permission denied if anonymous user
+#     if request.user.is_anonymous():
+#         raise PermissionDenied()
     
-@login_required #only allows access for logged users
-def myview(request):
-    return HttpResponse("Hello World")
+# @login_required #only allows access for logged users
+# def myview(request):
+#     return HttpResponse("Hello World")
 
-def testpermission(user): # returns true if if user is authenticated
-    if user.is_authenticated() and user.has_perm("myapp.change_category"):
-        return True
-    else:
-        return False
+# def testpermission(user): # returns true if if user is authenticated
+#     if user.is_authenticated() and user.has_perm("myapp.change_category"):
+#         return True
+#     else:
+#         return False
+
+# @user_passes_test(testpermission)
+# def change_ctg(request):
+
+# @permission_required("myapp.change_category")
+# def store_creator(request):
+
