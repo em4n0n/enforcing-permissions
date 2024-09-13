6ba2d084d0e069e28117ad321b90c697970dd612
em4n0n <58372945+em4n0n@users.noreply.github.com>
2024-09-13 15:27:42 -0700

    Deny permission if user is anonymous

--- myproject/myapp/views.py
+++ myproject/myapp/views.py
@@ -2,3 +2,7 @@ from django.shortcuts import render
 from django.core.exceptions import PermissionDenied
 
 # Create your views here.
+
+def myview(request):
+    if request.user.is_anonymous():
+        raise PermissionDenied()
